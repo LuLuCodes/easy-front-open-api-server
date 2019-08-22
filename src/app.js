be8c09678ceb82abda61329a3bd284791c97e642
qianqing <qian.qing@aliyun.com>
2019-08-22 13:48:01 +0800

    fix bug

--- src/app.js
+++ src/app.js
@@ -11,7 +11,6 @@ import connectRedis from 'connect-redis';
 
 import sessionConfig from './config/session-config';
 import corsConfig from './config/cors-config';
-import auth from './middleware/auth';
 import sign from './middleware/sign';
 import log from './log';
 import index from './routes/index';
@@ -61,11 +60,6 @@ app.post('*', async (req, res, next) => {
 app.get('/__webpack_hmr', function(req, res) {
   res.send('');
 });
-
-// 验证token
-app.use(
-  auth
-);
 
 // 验证签名
 app.use(
